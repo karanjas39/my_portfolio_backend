82987ed611b4c203a7fec3fc70d8089cfd191b8f
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-01-25 20:18:13 +0530

    rate limit update

--- app.js
+++ app.js
@@ -3,30 +3,16 @@ const app = express();
 const path = require("path");
 
 const helmet = require("helmet");
-const rateLimit = require("express-rate-limit");
 
 const adminRouter = require("./ROUTES/admin");
 const userRouter = require("./ROUTES/user");
 const unhandledRoutes = require("./UTILS/unhandledRoutes");
-const constants = require("./UTILS/constants");
 
 // CONFIGURATION
 app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 app.use(express.json({ limit: "12kb" }));
 app.use(express.urlencoded({ extended: true, limit: "12kb" }));
-app.use(
-  rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 100,
-    handler: (req, res) => {
-      res.send({
-        status: 400,
-        message: constants.tooManyRequests,
-      });
-    },
-  })
-);
 
 // ROUTES
 app.use("/api/v1/admin/developer", adminRouter);
