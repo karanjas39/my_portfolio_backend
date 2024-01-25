1b740b6c82dcfc9a6705b753fd5498b1f4481058
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-01-25 20:56:49 +0530

    routes update

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const helmet = require("helmet");
 const adminRouter = require("./ROUTES/admin");
 const userRouter = require("./ROUTES/user");
 const unhandledRoutes = require("./UTILS/unhandledRoutes");
+const contactRoutes = require("./CONTROLLERS/contact.controllers");
 const whitelist = [process.env.CORS_ORIGIN_USER, process.env.CORS_ORIGIN_ADMIN];
 const corsOptions = {
   origin: function (origin, callback) {
@@ -20,15 +21,17 @@ const corsOptions = {
 };
 
 // CONFIGURATION
-app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(helmet());
 app.use(express.json({ limit: "12kb" }));
 app.use(express.urlencoded({ extended: true, limit: "12kb" }));
 
 // ROUTES
-app.use("/api/v1/admin/developer", adminRouter);
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/admin/developer", cors(corsOptions), adminRouter);
+app.use("/api/v1/user", cors(corsOptions), userRouter);
+
+// FOR CONTACTS
+app.route("/api/v1/user/contact/add").post(contactRoutes.createContact);
 
 // UNHANDLED ROUTES
 app.route("*").all(unhandledRoutes.unhandleRoutes);
