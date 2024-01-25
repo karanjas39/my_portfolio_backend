b41b5ec821ab57f5d0f12063a71877f3365b8e41
karanjas39 <dhillonjaskaran4486@gmail.com>
2024-01-25 21:02:45 +0530

    route update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use("/api/v1/admin/developer", cors(corsOptions), adminRouter);
 app.use("/api/v1/user", cors(corsOptions), userRouter);
 
 // FOR CONTACTS
-app.route("/api/v1/contact/add").post(contactRoutes.createContact);
+app.use("/api/v1/contact/add", cors(), contactRoutes.createContact);
 
 // UNHANDLED ROUTES
 app.route("*").all(unhandledRoutes.unhandleRoutes);
